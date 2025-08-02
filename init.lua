2038542d86346ab9c19487ceb19ac044e8c2fd46
TAKE Takashi <nakamizo.takashi@gmail.com>
2025-08-02 17:55:09 +0900

    一旦動くところまで

--- init.lua
+++ init.lua
@@ -70,19 +70,47 @@ showLoadedModules(hs)
 function triggerTailscale(hs)
     -- iCloudの特定のフォルダを監視する
     local dirpath = os.getenv("HOME") .. "/Library/Mobile Documents/com~apple~CloudDocs/TriggerTailscale"
-    local watcher = hs.pathwatcher.new(dirpath, function(files)
-        for _, file_path in ipairs(files) do
-            local file_name = file_path:match("([^/]+)$") -- フルパスからファイル名を取得
+
+    -- ファイル監視
+    local watcher = hs.pathwatcher.new(dirpath, function(paths, flagTables)
+        for index, path in ipairs(paths) do
+            local file_name = path:match("([^/]+)$") -- フルパスからファイル名を取得
+            local event_flags = flagTables[index]
+
+            print("----- DEBUG -----")
+            print("file_name = " .. file_name)
+            print("event_flags = " .. hs.inspect(event_flags))
+            print("----- DEBUG -----")
+
+            -- ファイルのイベントを確認（ファイル作成、もしくはiCloud経由でファイル作成（リネーム、改変））
+            if event_flags.itemCreated or (event_flags.itemRenamed and event_flags.itemModified) then
+            else
+                goto continue
+            end
+
+            -- on.txtかつファイル作成イベントの場合
             if file_name == "on.txt" then
                 hs.notify.new({title="Tailscale トリガー", informativeText="on.txt を検知しました。tailscaled を起動します。"}):send()
                 print("Change: on.txt detected")
-                hs.execute("tailscale up")
-            elseif file_name == "off.txt" then
+                local output, status, typ, rc = hs.execute("screen -dmS tailscale_session tailscale up", true)
+                print("commad output = " .. output)
+                print("commad status = " .. hs.inspect(status))
+                os.remove(path) -- 処理後にファイルを削除（フルパスの方を指定すること）
+            end
+
+            -- off.txtかつファイル作成イベントの場合
+            if file_name == "off.txt" then
                 hs.notify.new({title="Tailscale トリガー", informativeText="off.txt を検知しました。tailscaled を停止します。"}):send()
                 print("Change: off.txt detected")
-                hs.execute("tailscale down")
+                local output, status, typ, rc = hs.execute("tailscale down", true)
+                print("commad output = " .. output)
+                print("commad status = " .. hs.inspect(status))
+                os.remove(path) -- 処理後にファイルを削除（フルパスの方を指定すること）
             end
+
+            ::continue::
         end
-    end):start()
+    end)
+    watcher:start()
 end
 triggerTailscale(hs)
