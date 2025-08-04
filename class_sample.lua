65377b620382dfefc5ffd367fea08799d706886c
TAKE Takashi <nakamizo.takashi@gmail.com>
2025-08-04 13:03:41 +0900

    インスタンス化に対応

--- class_sample.lua
+++ class_sample.lua
@@ -1,14 +1,20 @@
+local MenuManager = require("menu_manager")
+
 local ClassSample = {}
 ClassSample.__index = ClassSample
 
 function ClassSample:new()
-    -- 呼び出し元のLuaでpackage.pathがちゃんとしていれば問題なく呼べるらしい
-    local appMenu = require("menu_manager"):new()
+    -- インスタンス化のおまじない
+    local self = setmetatable({}, ClassSample)
 
-    appMenu:register("sample_app", {
+    self.AppMenu = MenuManager:new()
+
+    self.AppMenu:register("sample_app", {
         { title = "sample1", fn = function() hs.alert.show("sample!") end },
         { title = "sample2", fn = function() hs.alert.show("sample2!") end },
     })
+
+    return self
 end
 
 return ClassSample
